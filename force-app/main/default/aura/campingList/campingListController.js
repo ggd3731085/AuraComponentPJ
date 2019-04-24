05d7bc36f9b011d22c5a17a1f637e53ee253c1cf
ggd3731085 <ggd3731@gmail.com>
2019-04-24 21:53:01 +0900

    v3

--- force-app/main/default/aura/campingList/campingListController.js
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -1,3 +1,27 @@
+/*({
+    clickCreateItem: function (component, event, helper) {
+        var isFormValid = component.find("campingItemForm").reduce(function (isValid, inputCmp) {
+            inputCmp.showHelpMessageIfInvalid();
+            return isValid && inputCmp.get("v.validity").valid;
+        });
+
+        if (isFormValid) {
+            var newCampingItem = JSON.parse(JSON.stringify(component.get("v.newItem")));
+            console.log(JSON.parse(JSON.stringify(newCampingItem)), JSON.stringify(newCampingItem));
+            var campingItems = JSON.parse(JSON.stringify(component.get("v.items")));
+            campingItems.push(newCampingItem);
+            component.set("v.items", campingItems);
+            component.set("v.newItem", { 'Price__c': 0, 'Packed__c': false, 'Quantity__c': 0, 'Name': '', 'sobjectType': 'Camping_Item__c' });
+        }
+        component.set("v.newItem", {
+            'sobjectType': 'Camping_Item__c',
+            'Name': '',
+            'Quantity__c': 0,
+            'Price__c': 0,
+            'Packed__c': false
+        });
+    }
+})*/
 ({
 	clickCreateItem : function(component, event, helper) {
         var isFormValid = component.find("campingItemForm").reduce(function(isValid, inputCmp){
@@ -6,17 +30,19 @@
         });
         
         if (isFormValid) {
-            var newCampingItem = JSON.parse(JSON.stringify(component.get("v.newItem")));
-            console.log(JSON.parse(JSON.stringify(newCampingItem)), JSON.stringify(newCampingItem));
-            var campingItems = JSON.parse(JSON.stringify(component.get("v.items")));
-            campingItems.push(newCampingItem);
-            component.set("v.items",campingItems);
-            component.set("v.newItem", {'Price__c': 0, 'Packed__c': false, 'Quantity__c': 0, 'Name':'', 'sobjectType': 'Camping_Item__c'});
+            
+            var newCampingItem = component.get("v.newItem");
+            helper.createItem(component,newCampingItem);
+           
         }
-                component.set("v.newItem",{'sobjectType':'Camping_Item__c',
-                'Name': '',
-                'Quantity__c': 0,
-                'Price__c': 0,
-                'Packed__c': false});
-	}
+	},
+    
+    doInit : function (component, event, helper) {
+        var action = component.get("c.getItems");
+        action.setCallback(this,function (response) { 
+        	var campingItems = response.getReturnValue();
+            component.set("v.items",campingItems);
+        });
+        $A.enqueueAction(action);
+    }
 })
