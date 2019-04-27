d2722e28742cd03a55760a8f86d60299fb4c1be1
ggd3731085 <ggd3731@gmail.com>
2019-04-27 23:44:28 +0900

    V4

--- force-app/main/default/aura/campingList/campingListController.js
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -36,7 +36,21 @@
            
         }
 	},
-    
+    handleAddItem: function(component, event, helper) {
+        var newItem = event.getParam("item");
+        var action = component.get("c.saveItem");
+        action.setParams({"item": newItem});
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (component.isValid() && state === "SUCCESS") {
+                var items = component.get("v.items");
+                items.push(item);
+                component.set("v.items",items);
+            }
+        });
+        $A.enqueueAction(action);
+        
+    },
     doInit : function (component, event, helper) {
         var action = component.get("c.getItems");
         action.setCallback(this,function (response) { 
